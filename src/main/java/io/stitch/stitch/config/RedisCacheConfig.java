dd136eba3a45aa206e65f14dc5eed535d8003b03
ihabTawffiq <37032155+ihabTawffiq@users.noreply.github.com>
2024-11-09 21:05:23 +0200

    test connectivity

--- src/main/java/io/stitch/stitch/config/RedisCacheConfig.java
+++ src/main/java/io/stitch/stitch/config/RedisCacheConfig.java
@@ -1,5 +1,6 @@
 package io.stitch.stitch.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.cache.CacheManager;
 
 import org.springframework.context.annotation.Bean;
@@ -7,6 +8,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
 import java.time.Duration;
 
 @Configuration
@@ -14,14 +18,19 @@ public class RedisCacheConfig {
 
     @Bean
     public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        // Configure the ObjectMapper if necessary
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
 
         // Short-lived cache configuration
         RedisCacheConfiguration shortCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer))
                 .entryTtl(Duration.ofMinutes(1))
                 .disableCachingNullValues();
 
         // Long-lived cache configuration
         RedisCacheConfiguration longCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer))
                 .entryTtl(Duration.ofMinutes(5))
                 .disableCachingNullValues();
 
